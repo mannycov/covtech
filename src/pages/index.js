663c460de624228ec7d15f3acfcce09f35925f43
Emanuel <manny.cov09@gmail.com>
2019-06-30 12:07:31 -0700

    add github link in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ const IndexPage = () => {
     <Layout>
       <h1>COVTECH</h1>
       <h2>Emanuel Covarrubias from Silicon Valley</h2>
+      <p>Visit my GitHub page at <a href="https://github.com/mannycov" target="_blank" rel="noopener noreferrer">github.com/mannycov</a></p>
       <p>Visit the BeRelentless project details page <Link to="/berelentless">here</Link></p>
       <p>Visit the HFX project details page <Link to="/hfx">here</Link></p>
     </Layout>
