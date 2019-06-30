f51e19dd8e8cc0d4e7f1ce74ab3de1a243559bfc
Emanuel <manny.cov09@gmail.com>
2019-06-30 10:38:21 -0700

    add links to projects in index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const IndexPage = () => {
   return (
     <div>
       <h1>COVTECH</h1>
       <h2>Emanuel Covarrubias from Silicon Valley</h2>
+      <p>Visit the BeRelentless project details page <Link to="/berelentless">here</Link></p>
+      <p>Visit the HFX project details page <Link to="/hfx">here</Link></p>
     </div>
   )
 }
