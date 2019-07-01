f1e883f537a92c25e8dc9555a5c70e3c2bbb7d53
Emanuel <manny.cov09@gmail.com>
2019-06-30 21:09:17 -0700

    cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,11 @@ const IndexPage = () => {
   return (
     <Layout>
       <h2>Emanuel Covarrubias from Silicon Valley</h2>
-      <p>Visit my GitHub page at <a href="https://github.com/mannycov" target="_blank" rel="noopener noreferrer">github.com/mannycov</a></p>
+      <p>Visit my GitHub page at 
+        <a href="https://github.com/mannycov" target="_blank" rel="noopener noreferrer">
+          github.com/mannycov
+        </a>
+      </p>
       <p>Visit the BeRelentless project details page <Link to="/berelentless">here</Link></p>
       <p>Visit the HFX project details page <Link to="/hfx">here</Link></p>
     </Layout>
