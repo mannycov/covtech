d0b5306710171680c88f6c5e2eb3cb16258ccd00
Emanuel <manny.cov09@gmail.com>
2019-08-06 21:37:01 -0700

    edit projects grid edit smooth scroll offset for responsiveness

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,17 +5,31 @@ import AnchorLink from 'react-anchor-link-smooth-scroll'
 import navStyles from '../styles/nav.module.scss'
 
 const Nav = ({ title }) => {
-  let display;
+  let display, offSet;
   title === 'Home' ? display = 'flex' : display = 'none';
+
+  function handleOffSet() {
+    const windowWidth = window.innerWidth;
+    windowWidth <= 500 ? offSet = 110 : offSet = 80;
+  }
+
+  handleOffSet();
+
+  function resize() {
+    handleOffSet();
+  }
+
+  window.onresize = resize;
+
   return (
     <nav id={navStyles.mainNav}>
       <h1><Link className={navStyles.title} to="/">covtech</Link></h1>
       <ul style={{ display }} >
         <li><Link className={navStyles.navLink} activeClassName={navStyles.activeNavLink} to="/">Home</Link></li>
-        <li><AnchorLink className={navStyles.navLink} offset={() => 80} href="/#about">About</AnchorLink></li>
-        <li><AnchorLink className={navStyles.navLink} offset={() => 80} href="#projects">Projects</AnchorLink></li>
-        <li><AnchorLink className={navStyles.navLink} offset={() => 80} href="#technologies">Technologies</AnchorLink></li>
-        <li><AnchorLink className={navStyles.navLink} offset={() => 80} href="#contact">Contact</AnchorLink></li>
+        <li><AnchorLink className={navStyles.navLink} offset={() => offSet} href="/#about">About</AnchorLink></li>
+        <li><AnchorLink className={navStyles.navLink} offset={() => offSet} href="#projects">Projects</AnchorLink></li>
+        <li><AnchorLink className={navStyles.navLink} offset={() => offSet} href="#technologies">Technologies</AnchorLink></li>
+        <li><AnchorLink className={navStyles.navLink} offset={() => offSet} href="#contact">Contact</AnchorLink></li>
       </ul>
     </nav>
   )
